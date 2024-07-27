7784b16bd0d0be9b7fd230148d28ba9a6cddd717
E J <ej@Es-MacBook-Pro.local>
2024-07-27 14:10:53 -0600

    navbar

--- carecircle/src/App.js
+++ carecircle/src/App.js
@@ -7,22 +7,21 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className='main'>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/To-Do" element={<To-Do />} />
+        <Route path="/Crossy-Care" element={<CrossyCare />} />
+        <Route path="/Treatment" element={<Treatment />} />
+        <Route path="/Resources" element={<Resources />} />
+
+      </Routes>
+      
     </div>
+      <Footer/>
+  </BrowserRouter>
   );
 }
 

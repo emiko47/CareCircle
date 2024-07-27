f481883d752d6388074b3018da913a9226ece61e
Okibe Abang <okibe.abang@ucalgary.ca>
2024-07-27 16:32:46 -0600

    started with to do implementation

---
+++ carecircle/src/pages/ToDo/ToDo.js
@@ -0,0 +1,24 @@
+import React, { useRef, useState } from 'react';
+
+
+
+
+
+const ToDo = () => {
+    return (
+      <div className ='test'>bread and stew</div>
+    );
+  }
+  
+  export default ToDo;
+  
+
+
+  import React from 'react'
+  
+  export const ToDo = () => {
+    return (
+      <div>ToDo</div>
+    )
+  }
+  

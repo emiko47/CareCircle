33cb1e078561ae1882c5d7b37af36a0840c3170d
Okibe Abang <okibe.abang@ucalgary.ca>
2024-07-28 03:44:44 -0600

    added checkbox

--- carecircle/src/pages/ToDo/ToDo.js
+++ carecircle/src/pages/ToDo/ToDo.js
@@ -8,8 +8,14 @@ import {faTrash} from '@fortawesome/free-solid-svg-icons'
 export const ToDo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
+
+      <input type='checkbox'
+        checked={task.completed}
+        onChange={() => toggleComplete(task.id)}/>
+
       <p onClick={() => toggleComplete(task.id)}
       className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+
       <div>
          <FontAwesomeIcon className= 'edit-icon' icon={faPenToSquare} onClick={()=> editTodo(task.id)}/>
         <FontAwesomeIcon className = 'delete-icon' icon={faTrash} onClick={()=> deleteTodo(task.id)}/>
